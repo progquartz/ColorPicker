6c7c1e1acc6de8b545f974a4f3cade78af8f1ecf
progquartz <progquartz@gmail.com>
2021-03-17 01:37:45 +0900

    #정답 블럭 세트 만들기 - 블럭 세트를 만듬. - 정답 블럭 세트는 이를 상속받는 친구로써 만듬.

---
+++ ColorPicker2/Assets/Scripts/Block.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int x; // x좌표
+    public int y; // y좌표
+
+    public Color GetColor()
+    {
+        return GetComponent<SpriteRenderer>().color;
+    }
+
+    public Vector2 GetAxis()
+    {
+        return new Vector2(x, y);
+    }
+
+    public void SetColor(Color _color)
+    {
+        GetComponent<SpriteRenderer>().color = _color;
+    }
+
+    public void BasicSet(int _x, int _y, Color _color)
+    {
+        x = _x;
+        y = _y;
+        GetComponent<SpriteRenderer>().color = _color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ ColorPicker2/Assets/Scripts/BlockSet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSet : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject block;
+
+    public List<GameObject> Blocks;
+    public int MaxX;
+    public int MaxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartBlockSet(5,5);
+    }
+
+    /// <summary>
+    /// 블럭셋의 설정. 최대 X값과 , 최대 Y값의 데이터를 받아와 설정.
+    /// </summary>
+    /// <param name="_maxx">최대 X값</param>
+    /// <param name="_maxy">최대 Y값</param>
+    private void StartBlockSet(int _maxx, int _maxy)
+    {
+        float blocksize = block.GetComponent<SpriteRenderer>().bounds.size.x;
+        Vector3 blockPos = this.transform.position - new Vector3(MaxX * blocksize / 2, MaxY * blocksize / 2);
+        Transform BlockTrans = transform.GetChild(0);
+        for(int x = 0; x < MaxX; x++)
+        {
+            for(int y = 0; y < MaxY; y++)
+            {
+                Blocks.Add(Instantiate(block, blockPos, Quaternion.Euler(0, 0, 0)));
+                Blocks[x * MaxY + y].GetComponent<Block>().BasicSet(x, y, Color.white);
+                Blocks[x * MaxY + y].transform.SetParent(BlockTrans);
+                blockPos += new Vector3(0, blocksize);
+            }
+            blockPos.y = this.transform.position.y - (MaxY * blocksize / 2);
+            blockPos += new Vector3(blocksize, 0);
+        }
+    }
+
+    public Color GetBlockColor(int x, int y)
+    {
+        return Blocks[x * MaxY + y].GetComponent<Block>().GetColor();
+    }
+
+    public void SetBlockColor(int x, int y, Color _color)
+    {
+        Blocks[x * MaxY + y].GetComponent<Block>().SetColor(_color);
+    }
+}
