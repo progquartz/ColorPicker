480cfd236a091cc1bba3b1b30be72a45e9394f83
progquartz <progquartz@gmail.com>
2021-03-17 01:52:27 +0900

    normalfix_develop - develop이 안잡혀서 다시 작업.

--- ColorPicker2/Assets/Scripts/BlockSet.cs
+++ ColorPicker2/Assets/Scripts/BlockSet.cs
@@ -14,7 +14,7 @@ public class BlockSet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartBlockSet(5,5);
+        StartBlockSet(MaxX,MaxY);
     }
 
     /// <summary>
